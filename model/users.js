66b674e2798b57454d58bdd617d8370d2f5429bb
oR <tamiressenaisp@gmail.com>
2023-11-24 12:26:47 -0300

    inicializando

---
+++ model/users.js
@@ -0,0 +1,79 @@
+var usuario = {
+    clientes: 
+    [
+        {
+            imagem: '',
+            nome: 'Tamires Fernandes',
+            endereco_email: 'tamiresfernandes@gmail.com',
+            senha: 12345678,
+            endereco: 'Avenida Doutor Artur Costa Filho, 27 - Sumaré',
+            telefone:'(11) 95553-9563'
+        },
+        {
+            imagem: '',
+            nome: 'Alice Zeurgo',
+            endereco_email: 'alicezeurgo@gmail.com',
+            senha: 12345678,
+            endereco:'Rua Fortaleza - Jardim Brasilândia',
+            telefone:'(11) 97142-7270'
+        },
+        {
+            imagem: '',
+            nome: 'Luana Magalhães',
+            endereco_email: 'luanamaga@gmail.com',
+            senha: 12345678,
+            endereco:'Pc Mons Albino, 104',
+            telefone:'(11) 97539-3125'
+        },
+        {
+            imagem: '',
+            nome: 'Kaina Braga',
+            endereco_email: 'bragakaina@gmail.com',
+            senha: 12345678,
+            endereco:'Rua Washington Luís - Quiririm',
+            telefone:'(11) 95553-9563'
+        },
+        {
+            imagem: '',
+            nome: 'Maria Luiza Baliero',
+            endereco_email: 'mariabaliero@gmail.com',
+            senha: 12345678,
+            endereco:'Avenida Vinte e Nove de Abril, 350 - Centro',
+            telefone:'(11) 92698-3698'
+        },
+        {
+            imagem: '',
+            nome: 'Nathalia kawakami',
+            endereco_email: 'kawakaminat@gmail.com',
+            senha: 12345678,
+            endereco:'Avenida da Liberdade, 170',
+            telefone:'(11) 98574-6274'
+        },
+        {
+            imagem: '',
+            nome: 'ivy torquato',
+            endereco_email: 'madokaivy@gmail.com',
+            senha: 12345678,
+            endereco:'Rua Conceição Veloso, 48 - Vila Mariana',
+            telefone:'(11) 99492-1523'
+        },
+        {
+            imagem: '',
+            nome: 'Nelson Silva',
+            endereco_email: 'nelsinhofilho@gmail.com',
+            senha: 12345678,
+            endereco:'Rua Duarte de Azevedo, 258 - Santana',
+            telefone:'(11) 99685-5329'
+        },
+        {
+            imagem: '',
+            nome: 'Mylena Caetano',
+            endereco_email: 'caetanomymy@gmail.com',
+            senha: 12345678,
+            endereco:'R. Rosa Baraúna, 70 - Sta Marta, Xique-Xique - BA, 47400-000',
+            telefone:'(11) 95859-4000'
+        }
+    ],
+
+
+}
